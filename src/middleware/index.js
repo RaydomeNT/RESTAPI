bdb785b177b7a8aa54365b9b02b166b34abadd03
RaydomeNT <raydome85@gmail.com>
2022-08-03 13:23:03 +0100

    Fully working REST API with all CRUD functions

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,48 +3,51 @@ const jwt = require("jsonwebtoken");
 const User = require("../user/model");
 
 exports.hashPass = async (req, res, next) => {
-    try {
-        req.body.password = await bcrypt.hash(req.body.password, 8);
-        next();
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error });
-    }
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 8);
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error });
+  }
 }; //hashes the password, using 8 salt or "fuel"
 
 exports.comparePasswords = async (req, res, next) => {
   try {
-  //   const user = await User.findOne({ email: req.body.email });
-  //   const comparisonBool = await bcrypt.compare(req.body.password, user.password);
-  //   if (comparisonBool) {
-  //     req.user = user;
-  //     next();
-  //   } else {
-  //     throw new Error();
-  //   } old code, now condensed below
-    console.log("This is compare passwords")
-    req.user = await User.findOne({username: req.body.username});
-    if(req.user && (await bcrypt.compare(req.body.password, req.user.password))) {
+    //   const user = await User.findOne({ email: req.body.email });
+    //   const comparisonBool = await bcrypt.compare(req.body.password, user.password);
+    //   if (comparisonBool) {
+    //     req.user = user;
+    //     next();
+    //   } else {
+    //     throw new Error();
+    //   } old code, now condensed below
+    console.log("This is compare passwords");
+    req.user = await User.findOne({ username: req.body.username });
+    if (
+      req.user &&
+      (await bcrypt.compare(req.body.password, req.user.password))
+    ) {
       next();
     } else {
-      throw new Error({ msg: "Incorrect credentials" })
+      throw new Error({ msg: "Incorrect credentials" });
     }
   } catch (error) {
     console.log(error);
     res.status(500).send({ err: error });
   }
 }; //compare password works
-  
+
 exports.tokenAuth = async (req, res, next) => {
   try {
-      console.log("This is tokenCheck");
-      const token = req.header("Authorization");
-      const decodedToken = await jwt.verify(token, process.env.SECRET)
-      const user = await User.findById(decodedToken._id);
-      req.user = user;
-      next();
+    console.log("This is tokenCheck");
+    const token = req.header("Authorization");
+    const decodedToken = await jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decodedToken._id);
+    req.user = user;
+    next();
   } catch (error) {
-      console.log(error);
-      res.status(500).send({ err: error });
+    console.log(error);
+    res.status(500).send({ err: error });
   }
 }; //login get, using authorization in header.
--- src/user/controllers.js
+++ src/user/controllers.js
@@ -2,66 +2,66 @@ const jwt = require("jsonwebtoken");
 const User = require("./model");
 
 exports.createUser = async (req, res) => {
-    try {
-        const newUser = await User.create(req.body)
-        const token = await jwt.sign({_id: newUser._id}, process.env.SECRET); //creaes token with user id inside
-        //generate token using newUser_ID
-        console.log(newUser);
-        res.status(200).send({msg: "New user added", token }); //sendssuccess message and the token in the repsonse
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ err: error });
-    }
+  try {
+    const newUser = await User.create(req.body);
+    const token = await jwt.sign({ _id: newUser._id }, process.env.SECRET); //creaes token with user id inside
+    //generate token using newUser_ID
+    console.log(newUser);
+    res.status(200).send({ msg: "New user added", token }); //sendssuccess message and the token in the repsonse
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error });
+  }
 }; //create user works. /user with post in TC
 
 exports.login = async (req, res) => {
-    try {
-        const token = await jwt.sign({_id: req.user._id}, process.env.SECRET); //creates token with user id inside
-        //generate token using newUser_ID      
-        res.status(200).send({ user: req.user, token }); 
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error });
-    }
-}; //token auth works. 
+  try {
+    const token = await jwt.sign({ _id: req.user._id }, process.env.SECRET); //creates token with user id inside
+    //generate token using newUser_ID
+    res.status(200).send({ user: req.user, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error });
+  }
+}; //token auth works.
 
 //Mongoose read, update & delete
 exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find({});
-        const result = users.map((u) => {
-            return u.username;
-        });
-        res.status(200).send({ allUsers: result })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error });
-    }
+  try {
+    const users = await User.find({});
+    const result = users.map((u) => {
+      return u.username;
+    });
+    res.status(200).send({ allUsers: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error });
+  }
 }; //works in TC. use get & /list, no code needed in body, returns list of usernames.
 
 exports.updateEmail = async (req, res) => {
-    try {
-        const newEmail = await User.findOneAndUpdate(
-        { username: req.body.username },
-        { $set: { email: req.body.email } }
-        );
-        res.status(200).send({ message: "Email Updated" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error });
-    }
+  try {
+    const newEmail = await User.findOneAndUpdate(
+      { username: req.body.username },
+      { $set: { email: req.body.email } }
+    );
+    res.status(200).send({ message: "Email Updated" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error });
+  }
 }; //update email works in TC, patch
 //   {
 //     "username" : "enter user name",
 //     "email" : "enter new email address"
 //   }
-  
+
 exports.deleteUser = async (req, res) => {
-    try {
-        await User.findOneAndDelete({ username: req.body.username });
-        res.status(200).send({ message: "User Deleted" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error });
-    }
+  try {
+    await User.findOneAndDelete({ username: req.body.username });
+    res.status(200).send({ message: "User Deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error });
+  }
 }; //delete user works in TC. Delete /delete "username":"enter the username to be deleted"
